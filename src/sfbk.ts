d1635d6f2c2748f8128778d75612a6d50d56b627
my name <Yisheng.Jiang@gmail.com>
2021-04-07 01:08:06 +0000

    tt

---
+++ src/sfbk.ts
@@ -0,0 +1,50 @@
+import * as pdta_1 from './pdta.js';
+import { readAB } from './aba.js';
+
+export async function initsfbk(url) {
+  const arr = new Uint8Array(
+    await (
+      await fetch(url, { headers: { Range: 'bytes=0-6400' } })
+    ).arrayBuffer()
+  );
+
+  const r = readAB(arr);
+  const [riff, filesize, sig, list] = [
+    r.readNString(4),
+    r.get32(),
+    r.readNString(4),
+    r.readNString(4),
+  ];
+  console.assert(riff == 'RIFF' && sig == 'sfbk');
+
+  let infosize = r.get32();
+  console.log(r.readNString(4), r.offset);
+  console.log(infosize, r.offset);
+  let infoStart = r.offset;
+  r.skip(infosize - 4);
+  console.assert(r.readNString(4) == 'LIST');
+  const sdtaSize = r.get32();
+  const stdstart = r.offset + 8;
+  const pdtastart = stdstart + sdtaSize;
+  debugger;
+  const worker = new Worker('dist/worker.js', { type: 'module' });
+  worker.postMessage({
+    sdta: {
+      url,
+      range: [stdstart, pdtastart],
+    },
+  });
+  const parr = new Uint8Array(
+    await (
+      await fetch(url, {
+        headers: { Range: 'bytes=' + pdtastart + '-' },
+      })
+    ).arrayBuffer()
+  );
+  const pr = readAB(parr);
+  console.log(pr.readNString(4));
+  return {
+    pdta: new pdta_1.PDTA(pr),
+    worker,
+  };
+}
