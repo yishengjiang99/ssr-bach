63810b010ef36cfb63cb312fec3a0cb3f910f05d
my name <Yisheng.Jiang@gmail.com>
2021-04-12 19:24:23 +0000

    s

--- nodep/src/sdta.test.ts
+++
@@ -1,8 +0,0 @@
-import test from 'ava';
-import { RingBuffer } from './sdta';
-
-test('ringbuffer', (t) => {
-  const malloc = () => 0;
-  const rb = new RingBuffer(malloc);
-  t.is(rb.writeptr(0.3), rb.sr * 0.3);
-});
