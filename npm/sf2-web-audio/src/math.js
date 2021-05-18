031e185fb602aecc69ea3849cdab677918d51091
Yisheng Jiang <yishengjiang@yahoo.com>
2021-05-18 01:55:17 -0700

    s

--- npm/sf2-web-audio/src/math.js
+++
@@ -1,4 +0,0 @@
-export const attenuate2gain = (cent) => Math.pow(10, cent / -200);
-export const centone2hz = (cent) => Math.pow(2, cent / 1200) * 8.176;
-export const centibel2regularamp = (centible) => Math.pow(10, centible / 200);
-export const centtime2sec = (centtime) => Math.pow(2, centtime / 1200);
--- npm/sf2-web-audio/src/shared-state.js
+++
@@ -1,8 +0,0 @@
-export class SharedState {
-    constructor(buf, offset, struct) {
-        this.u8 = new Uint8Array(buf, offset);
-        this.u32 = new Uint32Array(buf, offset);
-        this.f32 = new Float32Array(buf, offset);
-        this._struct = struct;
-    }
-}
